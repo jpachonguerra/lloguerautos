8deabe83c78a18e7d80e2ccd487f829f896c9284
jpachonguerra <javier.pachon.guerra@gmail.com>
2025-03-28 15:56:17 +0100

    comit de la fita 3

--- lloguer/admin.py
+++ lloguer/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Automobil
+from .models import *
 
 # Register your models here.
 admin.site.register(Automobil)
+admin.site.register(Reserva)
--- lloguer/models.py
+++ lloguer/models.py
@@ -8,3 +8,19 @@ class Automobil(models.Model):
     matricula = models.CharField(max_length=10)
     def __str__(self):
         return f"{self.marca} {self.model} ({self.matricula})"
+    
+# crear modelo Reserva que referencie a Automobil i User
+# para evitar que se reserve el mismo automobil en un mismo dia se hace una clave unica con la combinación automobil y data_inici
+#esto se puede hacer con un unique_together
+
+class Reserva(models.Model):
+    automobil = models.ForeignKey(Automobil, on_delete=models.CASCADE)
+    usuari = models.ForeignKey(User, on_delete=models.CASCADE)
+    data_inici = models.DateField()
+    data_final = models.DateField()
+    
+    class Meta:
+        unique_together = ('automobil', 'data_inici')
+        
+    def __str__(self):
+        return f"Reserva de {self.automobil} per {self.usuari} del {self.data_inici} al {self.data_final}"
